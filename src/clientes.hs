48c5dcec39c53dfe1544834140e412ff62ed16f1
Luciano <lucianoserruya@gmail.com>
2018-05-28 23:58:24 -0300

    Agregar itinerarios
    El objetivo 3 y 4 de la segunda parte ya estarían

--- src/clientes.hs
+++ src/clientes.hs
@@ -4,3 +4,25 @@ rodri       = Cliente "Rodri" 55 [] []
 marcos      = Cliente "Marcos" 40 [rodri] []
 cristian    = Cliente "Cristian" 2 [] []
 ana         = Cliente "Ana" 120 [marcos, rodri] []
+robertoCarlos = Cliente "Roberto Carlos" 165 [] []
+
+mezclaExplosiva = Itinerario 
+    "Mezcla explosiva" 
+    2.5 
+    [(tomarTrago GrogXD),
+    (tomarTrago (Klusener "huevo")), 
+    (tomarTrago (Klusener "frutilla"))]
+itinerarioBasico = Itinerario 
+    "Itinerario básico"
+    5.0 
+    [(tomarTrago JarraLoca), 
+    (tomarTrago (Klusener "chocolate")), 
+    (rescatarse 2), 
+    (tomarTrago (Klusener "huevo"))]
+salidaDeAmigos = Itinerario 
+    "Salida de amigos"
+    1
+    [(tomarTrago (Soda 1)),
+    (tomarTrago Tintico),
+    (amigarse robertoCarlos),
+    (tomarTrago JarraLoca)]
